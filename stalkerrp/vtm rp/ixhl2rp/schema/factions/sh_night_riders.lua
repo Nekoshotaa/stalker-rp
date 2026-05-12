eb91295fdaef63aa1f408f3fa82ecd624aaf829d
Nekoshotaa <timur.ramaznov@gmail.com>
2026-05-12 17:58:12 +0500

    Create sh_night_riders.lua

---
+++ stalkerrp/vtm rp/ixhl2rp/schema/factions/sh_night_riders.lua
@@ -0,0 +1,22 @@
+FACTION.name = "Ночные Всадники"
+FACTION.description = "Банда Сородичей, гуля и их приближённых, скрывающаяся на фронтире."
+FACTION.color = Color(150, 125, 100, 255)
+FACTION.isDefault = true
+
+FACTION.models = {
+    "models/arthurmorganpm/arthurmorganpm.mdl",
+    "models/bill_williamson/bill_williamson.mdl",
+    "models/charles_smith/charles_smith.mdl",
+    "models/hosea_matthews/hosea_matthews.mdl",
+    "models/johnmarstonrdr2/johnmarstonrdr2.mdl",
+    "models/lenny_summers/lenny_summers.mdl",
+	"models/micah_bell/micah_bell.mdl",
+	"models/players/western_72.mdl",
+	"models/players/western_73.mdl",
+	"models/players/western_76.mdl",
+	"models/players/western_94.mdl",
+	"models/rdr2/campleader/dutchpm.mdl",
+	"models/sadieadler/sadieadlerpm.mdl"
+}
+
+FACTION_RIDERS = FACTION.index
